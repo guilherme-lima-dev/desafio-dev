c75880b44e764ae1ac10e3c3618afcc9144f7035
João Duque <bosco.duque@gmail.com>
2023-07-10 20:28:57 -0300

    feat: Adicionando Material UI

--- mercado/src/main.tsx
+++ mercado/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Login from './login'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <BrowserRouter>
+    <Login />
+  </BrowserRouter>
+);
