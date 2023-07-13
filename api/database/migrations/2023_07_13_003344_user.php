98b32c2d7af13d8bda574095f805093b2251966c
viict <viictoredu74@gmail.com>
2023-07-12 22:35:32 -0300

    feat: migration user

--- api/database/migrations/2023_07_13_003344_user.php
+++ api/database/migrations/2023_07_13_003344_user.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('pass');
             $table->timestamps();
         });
     }
