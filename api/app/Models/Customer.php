d96cbedd2190836ebec7a21ac2cbd3b81e07b83b
viict <viictoredu74@gmail.com>
2023-07-18 20:02:37 -0300

    feat: Entidade customer

---
+++ api/app/Models/Customer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+
+class Customer extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable, HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name', 'cpf','born_at', 'phone', 'email'
+    ];
+
+    public function purchases()
+    {
+        return $this->hasMany(Purchase::class);
+    }
+}
