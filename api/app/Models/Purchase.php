7fbe23bb0fc2e4345afa09bfda233964be49faff
viict <viictoredu74@gmail.com>
2023-07-18 19:59:22 -0300

    feat: Entidade purchase

---
+++ api/app/Models/Purchase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+
+class Purchase extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable, HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'nfe', 'discount', 'customer_id', 'data'
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function purchaseProduct()
+    {
+        return $this->hasMany(PurchaseProduct::class);
+    }
+}
