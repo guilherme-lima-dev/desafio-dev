ed5eb824ea20177ab3cd1046caba2758801adaf4
viict <viictoredu74@gmail.com>
2023-07-20 16:51:48 -0300

    feat: Seeder user

---
+++ api/database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('admin123'),
+            ],
+        ]);
+    }
+}
